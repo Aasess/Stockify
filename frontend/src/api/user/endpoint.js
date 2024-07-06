dea8f06c305a0ecf0384550e1c13690d9945602e
Ashish Neupane <ashishneupane1997@gmail.com>
2024-07-06 16:39:05 -0400

    feat: Implement user detail api

--- frontend/src/api/user/endpoint.js
+++ frontend/src/api/user/endpoint.js
@@ -3,7 +3,7 @@ const baseURL = {
 }
 
 export const UserEndPoint = {
-  userDetail: baseURL.user,
+  userDetail: `${baseURL.user}/detail`,
   register: `${baseURL.user}/register`,
   login: `${baseURL.user}/login`,
   resetPasswordLink: `${baseURL.user}/send-reset-password-email`,
