2e6edf4877710e94308b3d033de7cee05ea26091
Ashish Neupane <ashishneupane1997@gmail.com>
2024-07-22 23:24:50 -0400

    feat: Stock table completed

--- frontend/src/scenes/stock/index.jsx
+++ frontend/src/scenes/stock/index.jsx
@@ -24,7 +24,7 @@ const Stock = () => {
   const [showDeleteModal, setShowDeleteModal] = useState(false)
   const [currentStock, setCurrentStock] = useState({
     id: '',
-    item_id: '',
+    item: '',
     price: '',
     received_quantity: '',
   })
@@ -49,12 +49,21 @@ const Stock = () => {
     }
   }
 
+  const generateItemName = (id) => {
+    return items.find((item) => item.id === Number(id))?.item_name
+  }
+
   const handleSave = async () => {
     try {
+      const formData = {
+        item_id: Number(currentStock.item),
+        price: Number(currentStock.price),
+        received_quantity: Number(currentStock.received_quantity),
+      }
       if (isEdit) {
-        await StockAction.updateStockById(currentStock.id, currentStock)
+        await StockAction.updateStockById(currentStock.id, formData)
       } else {
-        await StockAction.createNewStock(currentStock)
+        await StockAction.createNewStock(formData)
       }
       fetchStocks()
       setShowModal(false)
@@ -74,13 +83,17 @@ const Stock = () => {
   }
 
   const openEditModal = (stock) => {
-    setCurrentStock(stock)
+    const newState = {
+      item: stock.item_id,
+      ...stock,
+    }
+    setCurrentStock(newState)
     setIsEdit(true)
     setShowModal(true)
   }
 
   const openCreateModal = () => {
-    setCurrentStock({ id: '', item_id: '', price: '', received_quantity: '' })
+    setCurrentStock({ id: '', item: '', price: '', received_quantity: '' })
     setIsEdit(false)
     setShowModal(true)
   }
@@ -95,13 +108,6 @@ const Stock = () => {
     setCurrentStock({
       ...currentStock,
       [name]: value,
-    })
-  }
-
-  const handleItemSelect = (item_id) => {
-    setCurrentStock({
-      ...currentStock,
-      item_id: item_id,
     })
   }
 
@@ -128,63 +134,66 @@ const Stock = () => {
                 </Button>
               </Card.Header>
               <Card.Body>
-                {stocks?.length > 0 ? <Table responsive striped bordered hover className="mb-0">
-                  <thead>
-                    <tr>
-                      <th>Item</th>
-                      <th>Price</th>
-                      <th>Received Quantity</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {stocks?.map((stock) => (
-                      <tr key={stock.id}>
-                        <td>{stock.item_id}</td>
-                        <td>{stock.price}</td>
-                        <td>{stock.received_quantity}</td>
-                        <td>
-                          <Dropdown>
-                            <Dropdown.Toggle
-                              as="div"
-                              style={{
-                                border: 'none',
-                                background: 'none',
-                                padding: 0,
-                                cursor: 'pointer',
-                              }}
-                            >
-                              <FontAwesomeIcon icon={faEllipsisV} />
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                              <Dropdown.Item
-                                onClick={() => openEditModal(stock)}
-                              >
-                                <FontAwesomeIcon
-                                  icon={faEdit}
-                                  className="me-2"
-                                />
-                                Edit
-                              </Dropdown.Item>
-                              <Dropdown.Item
-                                style={{ color: 'red' }}
-                                onClick={() => openDeleteModal(stock.id)}
-                              >
-                                <FontAwesomeIcon
-                                  icon={faTrash}
-                                  className="me-2"
-                                />
-                                Delete
-                              </Dropdown.Item>
-                            </Dropdown.Menu>
-                          </Dropdown>
-                        </td>
+                {stocks?.length > 0 ? (
+                  <Table responsive striped bordered hover className="mb-0">
+                    <thead>
+                      <tr>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>Received Quantity</th>
+                        <th></th>
                       </tr>
-                    ))}
-                  </tbody>
-                </Table>: <NoRecordFound />}
-                
+                    </thead>
+                    <tbody>
+                      {stocks?.map((stock) => (
+                        <tr key={stock.id}>
+                          <td>{generateItemName(stock.item_id)}</td>
+                          <td>{stock.price}</td>
+                          <td>{stock.received_quantity}</td>
+                          <td>
+                            <Dropdown>
+                              <Dropdown.Toggle
+                                as="div"
+                                style={{
+                                  border: 'none',
+                                  background: 'none',
+                                  padding: 0,
+                                  cursor: 'pointer',
+                                }}
+                              >
+                                <FontAwesomeIcon icon={faEllipsisV} />
+                              </Dropdown.Toggle>
+
+                              <Dropdown.Menu>
+                                <Dropdown.Item
+                                  onClick={() => openEditModal(stock)}
+                                >
+                                  <FontAwesomeIcon
+                                    icon={faEdit}
+                                    className="me-2"
+                                  />
+                                  Edit
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                  style={{ color: 'red' }}
+                                  onClick={() => openDeleteModal(stock.id)}
+                                >
+                                  <FontAwesomeIcon
+                                    icon={faTrash}
+                                    className="me-2"
+                                  />
+                                  Delete
+                                </Dropdown.Item>
+                              </Dropdown.Menu>
+                            </Dropdown>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                ) : (
+                  <NoRecordFound />
+                )}
               </Card.Body>
             </Card>
           </Col>
@@ -196,22 +205,23 @@ const Stock = () => {
           </Modal.Header>
           <Modal.Body>
             <Form>
-              <Form.Group controlId="formItemId">
-                <Form.Label>Item ID</Form.Label>
-                <Dropdown onSelect={handleItemSelect} className="mb-3">
-                  <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                    {currentStock.item_id || 'Select Item'}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    {items.map((item) => (
-                      <Dropdown.Item key={item.id} eventKey={item.id}>
-                        {item.id}
-                      </Dropdown.Item>
-                    ))}
-                  </Dropdown.Menu>
-                </Dropdown>
+              <Form.Group controlId="formItemId" className="mb-3">
+                <Form.Label>Item</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="item"
+                  value={currentStock.item}
+                  onChange={handleChange}
+                >
+                  <option value="">Select item</option>
+                  {items.map((item, index) => (
+                    <option key={index} value={item.id}>
+                      {item.item_name}
+                    </option>
+                  ))}
+                </Form.Control>
               </Form.Group>
-              <Form.Group controlId="formPrice">
+              <Form.Group controlId="formPrice" className="mb-3">
                 <Form.Label>Price</Form.Label>
                 <Form.Control
                   type="text"
@@ -221,7 +231,7 @@ const Stock = () => {
                   onChange={handleChange}
                 />
               </Form.Group>
-              <Form.Group controlId="formReceivedQuantity" className="mt-3">
+              <Form.Group controlId="formReceivedQuantity" className="mb-3">
                 <Form.Label>Received Quantity</Form.Label>
                 <Form.Control
                   type="number"
