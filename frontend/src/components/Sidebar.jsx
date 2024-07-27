089dc0f7f344149aaecd0f99613922c6298538d2
Ashish Neupane <ashishneupane1997@gmail.com>
2024-07-27 01:29:15 -0400

    design improved

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Nav } from 'react-bootstrap'
 import {
@@ -9,37 +9,77 @@ import {
   PackagePlus,
   PackageMinus,
 } from 'lucide-react'
+import { useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
+  const location = useLocation()
+  const [activeLink, setActiveLink] = useState('')
+
+  useEffect(() => {
+    setActiveLink(location.pathname.split('/')[1])
+  }, [location])
+
   return (
     <div className={`slide-in-menu show`}>
       <Nav className="flex-column p-4">
-        <Nav.Link as={Link} to="/" className="d-flex align-items-center">
-          <Gauge />
+        <Nav.Link
+          as={Link}
+          to="/"
+          className={`d-flex align-items-center gap-2 ${
+            activeLink === 'dashboard' ? 'active' : ''
+          }`}
+        >
+          <Gauge size={26} />
           <span className="ps-2 fs-5">Dashboard</span>
         </Nav.Link>
         <Nav.Link
           as={Link}
           to="/category"
-          className="d-flex align-items-center"
+          className={`d-flex align-items-center gap-2 ${
+            activeLink === 'category' ? 'active' : ''
+          }`}
         >
-          <SquareGanttChart />
-          <span className="ps-2 fs-5">Category</span>
+          <SquareGanttChart size={26} />
+          <span className="ps-2">Category</span>
         </Nav.Link>
-        <Nav.Link as={Link} to="/vendor" className="d-flex align-items-center">
-          <Building2 />
+        <Nav.Link
+          as={Link}
+          to="/vendor"
+          className={`d-flex align-items-center gap-2 ${
+            activeLink === 'vendor' ? 'active' : ''
+          }`}
+        >
+          <Building2 size={26} />
           <span className="ps-2 fs-5">Vendor</span>
         </Nav.Link>
-        <Nav.Link as={Link} to="/item" className="d-flex align-items-center">
-          <ShoppingBasket />
+        <Nav.Link
+          as={Link}
+          to="/item"
+          className={`d-flex align-items-center gap-2 ${
+            activeLink === 'item' ? 'active' : ''
+          }`}
+        >
+          <ShoppingBasket size={26} />
           <span className="ps-2 fs-5">Item</span>
         </Nav.Link>
-        <Nav.Link as={Link} to="/stock" className="d-flex align-items-center">
-          <PackagePlus />
+        <Nav.Link
+          as={Link}
+          to="/stock"
+          className={`d-flex align-items-center gap-2 ${
+            activeLink === 'stock' ? 'active' : ''
+          }`}
+        >
+          <PackagePlus size={26} />
           <span className="ps-2 fs-5">Stock</span>
         </Nav.Link>
-        <Nav.Link as={Link} to="/sale" className="d-flex align-items-center">
-          <PackageMinus />
+        <Nav.Link
+          as={Link}
+          to="/sale"
+          className={`d-flex align-items-center gap-2 ${
+            activeLink === 'sale' ? 'active' : ''
+          }`}
+        >
+          <PackageMinus size={26} />
           <span className="ps-2 fs-5">Sales</span>
         </Nav.Link>
       </Nav>
